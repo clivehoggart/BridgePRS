3605650be4ed065e3db76f34db9fc31cca272af3
Clive Hoggart <hoggac01@macbook-pro.wireless.mountsinai.org>
2022-05-11 17:08:46 -0400

    sweep -> stage1

--- bin/BridgePRS2.sh
+++ bin/BridgePRS2.sh
@@ -408,7 +408,7 @@ then
     rm $outdir/$pop2\_weighted_combined_var_explained.txt
     Rscript --vanilla ~/BridgePRS/bin/pred_combine_en.R \
 	    --pred1 stage2 \
-	    --pred2 sweep \
+	    --pred2 stage1 \
 	    --pred.dir2 $indir \
 	    --pop2 $pop2 \
 	    --outdir $outdir \
