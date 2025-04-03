15d2dde6536ac7d4226d1990a3bed7558e31a516
Clive Hoggart <clivehoggart@gmail.com>
2025-04-03 14:49:25 -0400

    ...

--- bridgeExampleRun_sumstats.sh
+++ bridgeExampleRun_sumstats.sh
@@ -33,8 +33,8 @@ ${src}/Bash/BridgePRS_sumstat.sh $src/Rscripts \
     --sumstats_se SE \
     --pheno_name y \
     --n_cores 20 \
-    --do_block_pop1 1 \
-    --do_sumstat_pop1 0 \
+    --do_block_pop1 0 \
+    --do_sumstat_pop1 1 \
     --do_block_pop2 0 \
     --do_sumstat_pop2 0 \
     --do_clump_pop1 0 \
