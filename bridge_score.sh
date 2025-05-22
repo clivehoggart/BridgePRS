2399c1e33d2b7c7b58af7fb6c37e01f815f4bacb
Clive Hoggart <clivehoggart@gmail.com>
2025-05-22 11:40:31 -0400

    ...

--- bridge_score.sh
+++ bridge_score.sh
@@ -8,7 +8,7 @@ do
     for chr in {1..22}
     do
 	plink --bfile $dir2/pop_${pop}/genotypes/chr$chr \
-	      --score $dir1/${pop}_snp_weights_${model[$i]}_model.dat 1 2 4 \
+	      --score $dir1/${pop}/snp_weights_${model[$i]}_model.dat 1 2 4 \
 	      --keep $dir2/pop_${pop}/phenotypes/${pop}_valid.dat \
 	      --out $dir1/preds/${pop}_${model[$i]}_chr$chr
     done
