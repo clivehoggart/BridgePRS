4b979d9f8bc91f517cb4eb94b7510fa5241e01e0
Clive Hoggart <clivehoggart@gmail.com>
2025-05-22 15:02:02 -0400

    ...

--- bridge_score.R
+++ bridge_score.R
@@ -7,11 +7,11 @@ pred <- list()
 pop <- 'EAS'
 pheno <- fread(paste0('data/pop_',pop,'/phenotypes/',pop,'_valid.dat'))
 for( i in 1:2 ){
-    tmp <- fread(paste0(dir,pop,'_',model[i],'_chr1.sscore'))
-    pred[[i]] <- data.frame( tmp$IID, tmp$SCORE1_AVG*tmp$ALLELE_CT )
+    tmp <- fread(paste0(dir,pop,'_',model[i],'_chr1.profile'))
+    pred[[i]] <- data.frame( tmp$IID, tmp$SCORE * tmp$CNT )
     for( chr in 2:22 ){
-        tmp <- fread(paste0(dir,pop,'_',model[i],'_chr',chr,'.sscore'))
-        pred[[i]][,2] <- pred[[i]][,2] + tmp$SCORE1_AVG * tmp$ALLELE_CT
+        tmp <- fread(paste0(dir,pop,'_',model[i],'_chr',chr,'.profile'))
+        pred[[i]][,2] <- pred[[i]][,2] + tmp$SCORE * tmp$CNT
     }
     print(paste(model[i],cor( pheno$y, pred[[i]][,2] )^2))
 }
