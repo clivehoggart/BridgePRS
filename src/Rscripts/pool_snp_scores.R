869b41cfc455e4846b61f7ffc0a529da6201ee95
Clive Hoggart <clivehoggart@gmail.com>
2025-09-26 11:38:11 -0400

    ...

--- src/Rscripts/pool_snp_scores.R
+++ src/Rscripts/pool_snp_scores.R
@@ -18,18 +18,19 @@ for( i in 1:2 ){
     tmp <- list()
     for( iter in 1:opt$n.folds ){
         tmp[[iter]] <- fread(paste0(opt$workdir,'/fold',iter,'/snp_weights_',model[i],'_model.dat'))
-        snps <- union( snps, tmp[[iter]]$V1 )
+        snps <- union( snps, tmp[[iter]]$SNP.id )
     }
     ensembl.model <- as.data.frame(matrix( ncol=4, nrow=length(snps), data=0 ))
     ensembl.model[,1] <- snps
     for( iter in 1:opt$n.folds ){
-        ptr <- match( tmp[[iter]]$V1, snps )
-        ensembl.model[ptr,2:3] <- c( tmp[[iter]]$V2, tmp[[iter]]$V3 )
-        ensembl.model[ptr,4] <- ensembl.model[ptr,4] + tmp[[iter]]$V4
+        ptr <- match( tmp[[iter]]$SNP.id, snps )
+        ensembl.model[ptr,2:3] <- c( tmp[[iter]]$ALLELE1, tmp[[iter]]$ALLELE0 )
+        ensembl.model[ptr,4] <- ensembl.model[ptr,4] + tmp[[iter]][,4]
     }
     ptr.use <- which(ensembl.model[,4]!=0)
     ensembl.model <- ensembl.model[ptr.use,]
-    write.table( ensembl.model,
-                paste0(opt$workdir,'/snp_weights_',model[i],'_model.dat'),
-                row.names=FALSE, col.names=FALSE, quote=FALSE )
+    colnames(ensembl.model) <- c('SNP.id', 'ALLELE1', 'ALLELE0', 'beta' )
+    fwrite( ensembl.model,
+           paste0(opt$workdir,'/snp_weights_',model[i],'_model.dat'),
+           row.names=FALSE, col.names=TRUE, sep=" " )
 }
